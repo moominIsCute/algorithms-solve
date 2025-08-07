9d4213d2d26e9e0938c9d226bfa2cbfab0e53c36
moominIsCute <seogho1234@naver.com>
2025-08-07 21:05:35 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int result = 1;
+        
+        if(num1 == num2){
+            return result;}
+        else{result= -1;
+            return result;
+        }
+        
+    }
+}
